e10573e9bf039b2b4d49c0dc38733f6f0e2081ea
Gvidas <gvisis@gmail.com>
2020-12-29 14:01:09 +0200

    changed-data

--- js/data.js
+++ js/data.js
@@ -1,16 +1,14 @@
 const account = [
-    { month: 10, income: 150 },
-    { month: 1, income: 200, expense: 50 },
-    { month: 3, income: 450, expense: 200 },
-    { month: 7, expense: 50 },
-    { month: 2, income: 50 },
-    { month: 5, income: 150, expense: 50 },
-    { month: 9, income: 700, expense: 400 },
-    { month: 4, income: 10, expense: 300 },
-    { month: 6, expense: 50 },
-    { month: 11, income: 150, expense: 30 },
-    { month: 8, income: 450, expense: 120 },
-    { month: 12, expense: 200 }
+    { month: 'Spalis', income: 150 },
+    { month: 'Sausis', income: 200, expense: 50 },
+    { month: 'Kovas', income: 450, expense: 200 },
+    { month: 'Liepa', expense: 50 },
+    { month: 'Vasaris', income: 50 },
+    { month: 'Geguze', income: 150, expense: 50 },
+    { month: 'Rugsejis', income: 700, expense: 400 },
+    { month: 'Balandis', income: 10, expense: 300 },
+    { month: 'Birzelis', expense: 50 },
+    { month: 'Lapkritis', income: 150, expense: 30 },
+    { month: 'Rugpjutis', income: 450, expense: 120 },
+    { month: 'Gruodis', expense: 200 }
 ];
-
-const months = ['Sausis', 'Vasaris', 'Kovas',' Balandis', 'Geguze', 'Birzelis','Liepia','Rugpjutis','Rugsejis','Spalis','Lapkritis','Gruodis']
